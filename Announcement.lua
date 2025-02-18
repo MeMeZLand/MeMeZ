566e555ee2ab5cdc1e2fb18f8d7186ed7dd5e0ce
MeMeZ <644770257@qq.com>
2025-02-19 03:19:14 +0800

    Update Announcement.lua

--- Announcement.lua
+++ Announcement.lua
@@ -1,1 +1,1 @@
-return "你们好, 这里是MeMeZ的作者, 很高兴认识你们! \n目前我们支持的游戏: \nForsaken-被遗弃🔴(未上传)\nGrow Your Country!-发展你的国家!🟢\n通用\n最后一次更新 2025/2/19"
+return "你们好, 这里是MeMeZ的作者, 很高兴认识你们! \n目前我们支持的游戏: \nForsaken-被遗弃🟢\nGrow Your Country!-发展你的国家!🟢\n通用\n最后一次更新 2025/2/19"
