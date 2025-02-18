3042e671bab03d96891f51255a2a787876c55f5a
MeMeZ <644770257@qq.com>
2025-02-19 03:18:20 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,8 @@
 if game:GetService("CoreGui"):FindFirstChild("MeMeZAnnouncement") then return end
 local Supports = {
     ["128017651370280"] = "Grow Your Country!.lua",
-    ["138804971020716"] = "Grow Your Country!.lua"
+    ["138804971020716"] = "Grow Your Country!.lua",
+    ["18687417158"] = "Forsaken.lua"
 }
 local Scriptname = Supports[tostring(game.PlaceId)] or "Universal.lua"
 getgenv().MeMeZStorage = "https://raw.githubusercontent.com/MeMeZLand/MeMeZ/refs/heads/main/"
