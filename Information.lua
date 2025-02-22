bf5abaf34a2c48ee0413b3da5785363dac706af3
MeMeZ <644770257@qq.com>
2025-02-22 18:14:24 +0800

    Create Information.lua

---
+++ Information.lua
@@ -0,0 +1,23 @@
+return {
+  Announcement = "你们好, 这里是MeMeZ的作者, 很高兴认识你们! \n目前我们支持的游戏: \nForsaken-被遗弃🟢\nGrow Your Country!-发展你的国家!🟢\n通用\n出现任何问题概不负责!\n最后一次更新 2025/2/19",
+  ExploitAnnouncement = "你们好, 这里是MeMeZ的作者们, 我们公布了MeMeZ漏洞利用版!\n目前我们支持的漏洞利用:\nCarbonEngine🟢\nACS r15 1.1🟢\nACS r15 1.2.5🟢\n出现任何问题概不负责!\n最后一次更新 2025/2/10",
+  Respects = {
+    ["nil"] = "nil",
+    ["nil"] = "nil"
+  },
+  Blacklist = {
+    ["-3395236573-"] = "no we hate u"
+  },
+  Creators = {
+    {
+        "MEMES",
+        "1977471722",
+        {"主要制作人员", "米斯拉", "QQ: 1977471722"}
+    },
+    {
+        "MeMeA",
+        "3567703786",
+        {"主要制作人员", "迷你队长", "QQ: 3567703786"}
+    }
+  }
+}
