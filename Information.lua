9947b4172f996327e9bd7894b14b93e6cdb29a5a
MeMeZ <644770257@qq.com>
2025-03-23 15:53:33 +0800

    Update Information.lua

--- Information.lua
+++ Information.lua
@@ -1,8 +1,6 @@
 return {
-  IAnnouncement = "你们好, 这里是MeMeZ的作者, 很高兴认识你们! \n目前我们支持的游戏: \nForsaken-被遗弃🟢\nGrow Your Country!-发展你的国家!🟢\n通用\n出现任何问题概不负责!\n最后一次更新 2025/2/19",
-  Announcement = "傻逼 谢安迪女士(百度副总裁的女儿) 一个外籍华人护着一个韩国明星开中国人的个人信息, 随随便便一群外国人就能获取到中国人的个人信息, 警察收钱帮出, 这群人真是一点良心都没有😡"
-  IExploitAnnouncement = "你们好, 这里是MeMeZ的作者, 我们公布了MeMeZ漏洞利用版!\n目前我们支持的漏洞利用:\nCarbonEngine🟢\nACS r15 1.1🟢\nACS r15 1.2.5🟢\n出现任何问题概不负责!\n最后一次更新 2025/2/10",
-  ExploitAnnouncement = "傻逼 谢安迪女士(百度副总裁的女儿) 一个外籍华人护着一个韩国明星开中国人的个人信息, 随随便便一群外国人就能获取到中国人的个人信息, 警察收钱帮出, 这群人真是一点良心都没有😡"
+  Announcement = "你们好, 这里是MeMeZ的作者, 很高兴认识你们! \n目前我们支持的游戏: \nForsaken-被遗弃🟢\nGrow Your Country!-发展你的国家!🟢\n通用\n出现任何问题概不负责!\n最后一次更新 2025/2/19",
+  ExploitAnnouncement = "你们好, 这里是MeMeZ的作者, 我们公布了MeMeZ漏洞利用版!\n目前我们支持的漏洞利用:\nCarbonEngine🟢\nACS r15 1.1🟢\nACS r15 1.2.5🟢\n出现任何问题概不负责!\n最后一次更新 2025/2/10",
   Respects = {
     ["nil"] = "nil",
     ["nil"] = "nil"
