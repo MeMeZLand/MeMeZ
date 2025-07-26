c3f4b5c6643db964e977f2b6a4e551e90bd8e719
MeMeZ <644770257@qq.com>
2025-07-26 16:29:25 +0800

    Update Information.lua

--- Information.lua
+++ Information.lua
@@ -1,6 +1,6 @@
 return {
   Announcement = "你们好, 这里是MeMeZ的作者, 很高兴认识你们! \n目前我们支持的游戏: \nForsaken-被遗弃🟢\nGrow Your Country!-发展你的国家!🟢\n通用\n出现任何问题概不负责!\n最后一次更新 2025/2/19",
-  ExploitAnnouncement = "你们好, 这里是MeMeZ的作者, 我们公布了MeMeZ漏洞利用版!\n目前我们支持的漏洞利用:\nCarbonEngine🟢\nACS r15 1.1🟢\nACS 1.7.6🟢\nACS r15 1.2.5🟢\nAC6 FE Sounds🟢\nTonkSpawner Radio🟢\n出现任何问题概不负责!\n最后一次更新 2025/7/24",
+  ExploitAnnouncement = "你们好, 这里是MeMeZ的作者, 我们公布了MeMeZ漏洞利用版!\n目前我们支持的漏洞利用:\nCarbonEngine🟢\nACS r15 1.1🟢\nACS 1.7.6🟢\nACS r15 1.2.5🟢\nACS2.0.1🟢\nAC6 FE Sounds🟢\nTonkSpawner Radio🟢\n出现任何问题概不负责!\n最后一次更新 2025/7/26",
   Respects = {
     ["nil"] = "nil",
     ["nil"] = "nil"
