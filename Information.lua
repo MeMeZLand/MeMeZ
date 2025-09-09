431abd26e50bada267335aa81d352c7328c2e87b
MeMeZ <644770257@qq.com>
2025-09-09 23:36:24 +0800

    Update Information.lua

--- Information.lua
+++ Information.lua
@@ -1,6 +1,6 @@
 return {
     ScriptName = "MeMeZ",
-    Announcement = "你们好, 这里是MeMeZ的作者, 很高兴认识你们! \n目前我们支持的游戏: \nForsaken-被遗弃🟢\nGrow Your Country!-发展你的国家!🟢\n通用\n出现任何问题概不负责!\n最后一次更新 2025/2/19",
+    Announcement = "你们好, 这里是MeMeZ的作者, 很高兴认识你们! \n目前我们支持的游戏: \nForsaken-被遗弃🟢\nGrow Your Country!-发展你的国家!🟢\nKSFU-杀戮连胜剑斗宇宙🟢\n通用\n出现任何问题概不负责!\n最后一次更新 2025/9/9",
     ExploitAnnouncement = "你们好, 这里是MeMeZ的作者, 我们公布了MeMeZ漏洞利用版!\n目前我们支持的漏洞利用:\nCarbonEngine🟢\nACS r15 1.1🟢\nACS 1.7.6🟢\nACS r15 1.2.5🟢\nACS2.0.1🟢\nAC6 FE Sounds🟢\nTonkSpawner Radio🟢\nRCS(垃圾GCS是RCS的换皮)🟢\n出现任何问题概不负责!\n最后一次更新 2025/7/26",
     Respects = {
       ["7818860510"] = true,
